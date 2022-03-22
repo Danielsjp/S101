ef192285ecfbeb3c1a8b0e0b7afbffb1f3c110d7
Dani Sán <40555221+Danielsjp@users.noreply.github.com>
2022-03-22 18:49:58 +0100

    .
    .

---
+++ src/n1exercici4/Principal.java
@@ -0,0 +1,11 @@
+package n1exercici4;
+
+public class Principal {
+
+public static void main(String[] args) {
+
+Vehicle coche1 = new Vehicle();
+Vehicle coche2 = new Vehicle();
+new Vehicle();
+	}
+}
