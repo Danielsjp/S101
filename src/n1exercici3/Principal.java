d15b7e887aa283278686ea1b6147921c54a0d5e6
Dani Sán <40555221+Danielsjp@users.noreply.github.com>
2022-03-22 17:40:18 +0100

    .
    .

---
+++ src/n1exercici3/Principal.java
@@ -0,0 +1,11 @@
+package n1exercici3;
+
+public class Principal {
+
+public static void main(String[] args) {
+
+Vehicle coche1 = new Vehicle();
+Vehicle coche2 = new Vehicle();
+new Vehicle();
+	}
+}
---
+++ src/n1exercici3/Vehicle.java
@@ -0,0 +1,35 @@
+package n1exercici3;
+
+public class Vehicle {
+	
+	String Nom;
+	Integer Velocitat;
+	String Estat;
+
+	
+	public Vehicle()
+	{
+		System.out.println("Constructor");  
+	}
+	
+	{
+		System.out.println("Bloque de inicializacón");  
+	}
+	
+	public static void Iniciar() {
+		System.out.println("El coche esta arrancado");  
+	}
+	
+	public static void parar(){
+	    System.out.println ("El coche esta parando");
+	    
+	}
+	public void acelerar(){
+	    System.out.println ("El coche esta acelerando");
+	   
+	}
+
+}
+
+
+
