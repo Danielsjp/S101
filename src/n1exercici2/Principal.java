6be8236b12f009fb82b1d2af2ef4f25e81b39c30
Dani Sán <40555221+Danielsjp@users.noreply.github.com>
2022-03-22 17:24:15 +0100

    .
    .

---
+++ src/n1exercici2/Principal.java
@@ -0,0 +1,15 @@
+package n1exercici2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Vehicle.Iniciar();
+Vehicle.parar();
+Vehicle coche1 = new Vehicle();
+coche1.Iniciar();
+coche1.parar();
+coche1.acelerar();
+	}
+
+}
---
+++ src/n1exercici2/Vehicle.java
@@ -0,0 +1,31 @@
+package n1exercici2;
+
+public class Vehicle {
+	
+	String Nom;
+	Integer Velocitat;
+	String Estat;
+	
+	public Vehicle(){
+		
+		
+	}
+	
+	
+	public static void Iniciar() {
+		System.out.println("El coche esta arrancado");  
+	}
+	
+	public static void parar(){
+	    System.out.println ("El coche esta parando");
+	    
+	}
+	public void acelerar(){
+	    System.out.println ("El coche esta acelerando");
+	   
+	}
+
+}
+
+
+
