2c2899269a7532890b1f59154b4f3dbfa5faebaa
Dani Sán <40555221+Danielsjp@users.noreply.github.com>
2022-03-23 09:14:35 +0100

    .
    .

--- src/n1exercici4/Vehicle.java
+++ src/n1exercici4/Vehicle.java
@@ -2,15 +2,26 @@ package n1exercici4;
 
 public class Vehicle {
 	
-	String Nom;
-	Integer Velocitat;
-	String Estat;
+	static final String Nom="Unknow"; //requiere ser inicializado ( ES UNA CONSTANTE )
+	final Integer Velocitat; //es una constante, pero no requiere ser inicializado. cuando tenga un valor no se puede cambiar
+	static String Estat="no se sabe"; // se puede inicializar aqui o en el constructor, el valor puede cambiar.
 
 	
 	public Vehicle()
 	{
+		
+		Velocitat = 10;
+		Estat = "parado";
 		System.out.println("Constructor");  
 	}
+	
+	public void Vehicle2()
+	{
+		Estat = "encendido";
+		// Velocitat = 11; // no permite ser cambiado
+		System.out.println("Constructor2");  
+	}
+	
 	
 	{
 		System.out.println("Bloque de inicializacón");  
